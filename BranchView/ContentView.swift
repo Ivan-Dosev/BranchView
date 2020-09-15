819df9c9e8364894400124f6dce71097d98814de
Ivan <biobeehive.com>
2020-09-15 04:49:09 +0300

    Initial Commit

---
+++ BranchView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BranchView
+//
+//  Created by Ivan Dosev Dimitrov on 15.09.20.
+//  Copyright © 2020 Ivan Dosev Dimitrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
