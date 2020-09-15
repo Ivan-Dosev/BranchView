f3e4cc3aa3a1d5f8aeb43ddb428f089666c7d6a1
Ivan <biobeehive.com>
2020-09-15 21:25:38 +0300

    varna view

---
+++ BranchView/VarnaView.swift
@@ -0,0 +1,29 @@
+//
+//  VarnaView.swift
+//  BranchView
+//
+//  Created by Ivan Dosev Dimitrov on 15.09.20.
+//  Copyright © 2020 Ivan Dosev Dimitrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct VarnaView: View {
+    var body: some View {
+        HStack {
+            HStack {
+
+                  Text("😡")
+                  Text("🤭")
+                  Text("😰")
+                  Text("😍")
+            }
+        }
+    }
+}
+
+struct VarnaView_Previews: PreviewProvider {
+    static var previews: some View {
+        VarnaView()
+    }
+}
