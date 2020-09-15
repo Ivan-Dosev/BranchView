c6afafd20c71b1a5f900a1d85ed6f496ca44cb67
Ivan <biobeehive.com>
2020-09-15 19:55:27 +0300

    mecho View

---
+++ BranchView/MechoView.swift
@@ -0,0 +1,21 @@
+//
+//  MechoView.swift
+//  BranchView
+//
+//  Created by Ivan Dosev Dimitrov on 15.09.20.
+//  Copyright © 2020 Ivan Dosev Dimitrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct MechoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MechoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MechoView()
+    }
+}
